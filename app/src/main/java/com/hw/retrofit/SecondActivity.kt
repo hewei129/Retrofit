4b7a5998285f0ea546d1fd12dff13a2ea94e1851
David <hewei1390739@163.com>
2021-01-13 16:38:06 +0800

    新增支持带https 前缀的图片地址加载库

--- app/src/main/java/com/hw/retrofit/SecondActivity.kt
+++ app/src/main/java/com/hw/retrofit/SecondActivity.kt
@@ -2,18 +2,22 @@ package com.hw.retrofit
 
 import android.app.Activity
 import android.os.Bundle
+import com.hw.net.glide.imgLoad
+import kotlinx.android.synthetic.main.activity_second.*
 
 
 /**
  * @author hewei(David)
  * @date 2020/3/25  10:55 AM
  * @Copyright ©  Shanghai Yejia Digital Technology Co., Ltd.
- * @description
+ * @description 测试带https前缀的图片地址
  */
 
 class SecondActivity : Activity() {
+    private val pic_url = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.tukexw.com%2Fimg%2F88f553ad691dd61f.jpg&refer=http%3A%2F%2Fimg.tukexw.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613118834&t=6f982cc71d24ab1f390c640b54aaef4e"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_second)
+        imgLoad(this, pic_url, iv_photo)
     }
 }
